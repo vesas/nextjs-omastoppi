df163ab7afcbd3691a0928c811888bf195e93d98
Vesa <42914624+vesas@users.noreply.github.com>
2023-07-02 14:53:24 +0300

    test

--- app/stop.tsx
+++ app/stop.tsx
@@ -6,6 +6,7 @@ function Stop(props) {
     return (
         <div>
             <div className="bg-orange-500">Stop {props.id}</div>
+
             
             {props.stop.trips.map((trip) => {
             return <Trip key={trip.realTimeDeparture} trip={trip} />
